b189dee80fcf97220a5164ec9bdd81f5755c78e0
nptnk <174674969+nptnk@users.noreply.github.com>
2025-06-25 21:32:43 +1000

    camera shake when 5 seconds remaining on bing bong timer

--- MoreAscents/Ascents/Gimmicks/Chaos/BingBongGimmick.cs
+++ MoreAscents/Ascents/Gimmicks/Chaos/BingBongGimmick.cs
@@ -14,19 +14,33 @@ public class BingBongGimmick : AscentGimmick {
         return "Chaos 4";
     }
 
+    private static float SinceLastShake = 0;
+    
     public override void Update() {
-        if (BingBongPatches.Update.BingBongUnheldFor >= 15) {
-            foreach (Character character in Character.AllCharacters) {
-                if (!character.IsLocal) {
-                    continue;
-                }
-                if (character.data.passedOutOnTheBeach > 0) {
-                    BingBongPatches.Update.BingBongUnheldFor = 0;
-                    break;
-                }
-                character.refs.afflictions.AddStatus(CharacterAfflictions.STATUSTYPE.Crab,
-                    0.05f * Time.deltaTime, false);
+        SinceLastShake += Time.deltaTime;
+
+
+        foreach (Character character in Character.AllCharacters) {
+            if (!character.IsLocal) {
+                continue;
             }
+            if (character.data.passedOutOnTheBeach > 0) {
+                BingBongPatches.Update.BingBongUnheldFor = 0;
+                break;
+            }
+            
+            if (BingBongPatches.Update.BingBongUnheldFor > 10 && BingBongPatches.Update.BingBongUnheldFor <= 15) {
+                if (SinceLastShake > 1 / 60) {
+                    SinceLastShake = 0;
+                    GamefeelHandler.instance.AddPerlinShake(0.14f,0.15f,4f);
+                }
+            }
+
+            if (BingBongPatches.Update.BingBongUnheldFor < 15) {
+                continue;
+            }
+            character.refs.afflictions.AddStatus(CharacterAfflictions.STATUSTYPE.Crab, 0.05f * Time.deltaTime, false);
+            break;
         }
     }
 }
